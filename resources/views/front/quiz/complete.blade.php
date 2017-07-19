ef9bd15a9b14db1dd509d80b09bf9ca055bcdd6d
Gus <yo@soygus.com>
2017-07-19 17:55:04 -0500

    Updates complete template cntent

--- resources/views/front/quiz/complete.blade.php
+++ resources/views/front/quiz/complete.blade.php
@@ -1,13 +1,31 @@
 @extends('layouts.quiz')
 
 @section('head-link')
-  <link href="/css/front/quiz/new.css" rel="stylesheet">
+  <link href="/css/front/quiz/section.css" rel="stylesheet">
 @endsection
 
 @section('content')
   <div class="container" id="front-quiz-new">
-
-    <h1>Completo</h1>
+    <section class="section">
+      <header class="section-header"></header>
+      <div class="section-content">
+        <div class="container-md">
+          <h1>Has terminado el cuestionario</h1>
+          <h2>Asegúrate de verificar tu correo electrónico para que podamos contactarte</h2>
+          <h5>Si no lo has recibido revisa tu bandeja de correo no deseado ó da clic en</h5>
+          <form action="">
+            <button type="submit" class="btn btn-info btn-lg">Reenviar correo</button>
+          </form>
+          <hr>
+          <h3>Comparte este link con tus amigos y obtén descuentos en tus compras</h3>
+          <fieldset class="form-group">
+            <input type="text" class="form-control form-control-lg" value="URL">
+          </fieldset>
+          <p>* El descuento es válido sólo hasta que los usuarios que referiste realicen una compra</p>
+        </div>
+      </div>
+      <footer class="section-footer"></footer>
+    </section>
 
   </div>
 @endsection
