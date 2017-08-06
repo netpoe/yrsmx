cc03e2e844d8ba9105c564c10db9a13809b3cc78
Gus <yo@soygus.com>
2017-08-06 16:16:05 -0500

    Removes user_id from rel_products_outfit table

--- database/migrations/2017_07_05_141056_create_rel_products_outfit_table.php
+++ database/migrations/2017_07_05_141056_create_rel_products_outfit_table.php
@@ -17,14 +17,12 @@ class CreateRelProductsOutfitTable extends Migration
             $table->increments('id');
             $table->integer('outfit_id')->unsigned();
             $table->integer('product_id')->unsigned();
-            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
 
         Schema::table('rel_products_outfit', function (Blueprint $table) {
             $table->foreign('outfit_id')->references('id')->on('outfits');
             $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -40,9 +38,6 @@ class CreateRelProductsOutfitTable extends Migration
         });
         Schema::table('rel_products_outfit', function (Blueprint $table) {
             $table->dropForeign(['product_id']);
-        });
-        Schema::table('rel_products_outfit', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
         });
 
         Schema::dropIfExists('rel_products_outfit');
